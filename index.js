ff4ddf8651bd2b2994068ba233c928bcf7b38eb5
Klkgang <deniscortezsar@gmail.com>
2023-10-14 20:42:48 -0600

    version 1.1 de el login

--- index.js
+++ index.js
@@ -7,21 +7,39 @@ function validarUsuario() {
     errorMessage.style.display = 'block';
   } else {
     errorMessage.style.display = 'none';
-    // Aquí puedes enviar el nombre de usuario al servidor para su verificación
-    // Por ejemplo: enviarUsuarioAlServidor(usuario);
-    const valid = true;  
-    console.log('Nombre de usuario válido:', usuario);
+
+    const url = 'http://localhost:5000/endpoint'; // Ajusta la URL según tu configuración
+    const data = { usuario: usuario };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Respuesta del servidor Flask:', data);
+        if (data.message === 'Datos recibidos correctamente') {
+          // Aquí puedes agregar la lógica para redirigir a otra página o mostrar un mensaje de éxito
+          alert('Datos recibidos correctamente en el servidor.');
+        } else {
+          // Aquí puedes agregar la lógica para manejar un caso de error
+          alert('Hubo un problema al enviar los datos al servidor.');
+        }
+      })
+      .catch(error => console.error('Error:', error));
   }
 }
 
 function detectarPlataforma() {
   const anchoVentana = window.innerWidth;
-  const agenteUsuario = window.navigator.userAgent;
 
   if (anchoVentana >= 768) {
-    alert("Parece que estas en una computadora");
+    alert("Parece que estás en una computadora");
   } else {
-    alert("Parece que estas en un dispositivo movil");
+    alert("Parece que estás en un dispositivo móvil");
   }
 }
 
