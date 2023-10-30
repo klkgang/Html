7bfbcd43a94cf9745b987471cc5b28291a3f1275
Denis Cortez <139710771+klkgang@users.noreply.github.com>
2023-10-30 21:00:41 +0000

    working on progress

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
-function validarUsuario() {
-  const usuarioInput = document.getElementById('usuario');
-  const errorMessage = document.getElementById('error-msg');
-  const usuario = usuarioInput.value;
+const words1 = ["Easy to use", "User-friendly", "Intuitive"];
+const textDiv1 = document.getElementById("text_1");
+let i1 = 0;
+let j1 = 0;
 
-  if (usuario.includes(' ')) {
-    errorMessage.style.display = 'block';
-  } else {
-    errorMessage.style.display = 'none';
-
-    const url = 'http://localhost:5000/endpoint'; // Ajusta la URL según tu configuración
-    const data = { usuario: usuario };
-
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    };
-
-    fetch(url, requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        console.log('Respuesta del servidor Flask:', data);
-        if (data.message === 'Datos recibidos correctamente') {
-          // Aquí puedes agregar la lógica para redirigir a otra página o mostrar un mensaje de éxito
-          alert('Datos recibidos correctamente en el servidor.');
-        } else {
-          // Aquí puedes agregar la lógica para manejar un caso de error
-          alert('Hubo un problema al enviar los datos al servidor.');
-        }
-      })
-      .catch(error => console.error('Error:', error));
-  }
+// Function to type the words (all the rest are  the same)
+function typeWriter1() {
+    if (j1 < words1[i1].length) {
+        textDiv1.innerHTML += words1[i1].charAt(j1);
+        j1++;
+        setTimeout(typeWriter1, 60);
+    } else {
+        setTimeout(() => {
+            textDiv1.innerHTML = "";
+            i1++;
+            j1 = 0;
+            if (i1 >= words1.length) {
+                i1 = 0; 
+            }
+            typeWriter1();
+        }, 1500); 
+    }
 }
 
-function detectarPlataforma() {
-  const anchoVentana = window.innerWidth;
 
-  if (anchoVentana >= 768) {
-    alert("Parece que estás en una computadora");
-  } else {
-    alert("Parece que estás en un dispositivo móvil");
-  }
+const words2 = ["Practical", "Efficient", "Effective"];
+const textDiv2 = document.getElementById("text_2");
+let i2 = 0;
+let j2 = 0;
+
+function typeWriter2() {
+    if (j2 < words2[i2].length) {
+        textDiv2.innerHTML += words2[i2].charAt(j2);
+        j2++;
+        setTimeout(typeWriter2, 60);
+    } else {
+        setTimeout(() => {
+            textDiv2.innerHTML = "";
+            i2++;
+            j2 = 0;
+            if (i2 >= words2.length) {
+                i2 = 0; 
+            }
+            typeWriter2();
+        }, 1500); 
+    }
 }
 
+const words3 = ["Secure", "Protected", "Safe"];
+const textDiv3 = document.getElementById("text_3");
+let i3 = 0;
+let j3 = 0;
+
+function typeWriter3() {
+    if (j3 < words3[i3].length) {
+        textDiv3.innerHTML += words3[i3].charAt(j3);
+        j3++;
+        setTimeout(typeWriter3, 60);
+    } else {
+        setTimeout(() => {
+            textDiv3.innerHTML = "";
+            i3++;
+            j3 = 0;
+            if (i3 >= words3.length) {
+                i3 = 0; 
+            }
+            typeWriter3();
+        }, 1500); 
+    }
+}
+
+// Call the functions
+typeWriter1();
+typeWriter2();
+typeWriter3();
+console.log("It's working!");//random messaje in the console
